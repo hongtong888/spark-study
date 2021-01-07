b06b000913bac6a098a5d3fe25ce5711215ddab0
tong_hong <tong_hong@kingdee.com>
2021-01-07 11:03:46 +0800

    spark学习

--- spark-core/src/main/scala/com/atguigu/sparkcore/day02/Test.scala
+++ spark-core/src/main/scala/com/atguigu/sparkcore/day02/Test.scala
@@ -10,22 +10,22 @@ import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 object Test {
     
     def main(args: Array[String]): Unit = {
-//        val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
-//        val sc = new SparkContext(conf)
-//        val rdd1 = sc.parallelize(Array(30, 60, 90))
-//        val pairRDD: RDD[(Int, Int)] = rdd1.map((_, 1))
-//        println(pairRDD.partitions.length)
-//        val rdd2: RDD[(Int, Int)] = pairRDD.partitionBy(new HashPartitioner(3))
-//        println(rdd2.partitions.length)
-//        var rdd3 = pairRDD.mapPartitionsWithIndex {
-//            case (index, it) => {
-//                it.map {
-//                    case (num, one) => {
-//                        (index, (num, one))
-//                    }
-//                }
-//            }
-//        }
-//        println(rdd3.collect.mkString(" : "))
+        val conf = new SparkConf().setAppName("Practice").setMaster("local[2]")
+        val sc = new SparkContext(conf)
+        val rdd1 = sc.parallelize(Array(30, 60, 90))
+        val pairRDD: RDD[(Int, Int)] = rdd1.map((_, 1))
+        println(pairRDD.partitions.length)
+        val rdd2: RDD[(Int, Int)] = pairRDD.partitionBy(new HashPartitioner(3))
+        println(rdd2.partitions.length)
+        var rdd3 = pairRDD.mapPartitionsWithIndex {
+            case (index, it) => {
+                it.map {
+                    case (num, one) => {
+                        (index, (num, one))
+                    }
+                }
+            }
+        }
+        println(rdd3.collect.mkString(" : "))
     }
 }
